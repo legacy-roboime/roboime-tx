3124e99e85cb755db176677141493a451192bab6
RenanPicoli <renanps123@gmail.com>
2016-07-24 00:44:45 -0300

    removi o delay de main; transfere 250 pacotes/s

--- src/main.cpp
+++ src/main.cpp
@@ -86,8 +86,7 @@ int main(void)
 			if(send(&radio)){
 					packets_sent++;
 					STM_EVAL_LEDToggle(LED6);//AZUL:indicador de sucesso
-					Delay_ms(05);
-					//for (uint64_t i=0;i<0xfffff;i++);
+					//Delay_ms(05);
 			}
 			else{
 					STM_EVAL_LEDToggle(LED5);//VERMELHO:indicador de falha

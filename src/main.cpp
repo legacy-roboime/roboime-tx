6e649c3701f421c0c65678ea2b19d167135b61ad
RenanPicoli <renanps123@gmail.com>
2016-07-24 00:29:42 -0300

    delay de 5ms em main, 111 pacotes/s

--- src/main.cpp
+++ src/main.cpp
@@ -86,7 +86,7 @@ int main(void)
 			if(send(&radio)){
 					packets_sent++;
 					STM_EVAL_LEDToggle(LED6);//AZUL:indicador de sucesso
-					Delay_ms(10);
+					Delay_ms(05);
 					//for (uint64_t i=0;i<0xfffff;i++);
 			}
 			else{

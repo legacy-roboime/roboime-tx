b5086d3fb6193ce9d1464d451cbd273303810bf5
Gustavo Claudio Karl Couto <gustavokcouto@uol.com.br>
2016-08-04 02:13:29 -0300

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,6 @@
 /* Includes */
 #include "main.h"
 
-#include "own_libraries/NRF24.h"
-#include "own_libraries/CONFIG.h"
-#include "own_libraries/SPI_interface.h"
-
 uint32_t TimingDelay;
 
 //void Delay (uint32_t nTime);
@@ -31,23 +27,9 @@ extern "C" {
  void OTG_FS_WKUP_IRQHandler(void);
 }
 
-/*
- * The USB data must be 4 byte aligned if DMA is enabled. This macro handles
- * the alignment, if necessary (it's actually magic, but don't tell anyone).
- * See: http://electronics.stackexchange.com/questions/134474/how-to-get-usart-and-usb-serial-working-on-the-stm32f4discovery-board
- * See also usb_conf.h
- */
+
 __ALIGN_BEGIN USB_OTG_CORE_HANDLE  USB_OTG_dev __ALIGN_END;
 
-/**
-**===========================================================================
-**
-**  Abstract: main program
-**
-**===========================================================================
-*/
-
-uint8_t USB_get_and_send(NRF* radio_ptr);
 
 int main(void)
 {
@@ -62,89 +44,15 @@ int main(void)
   *  E.g.  SCB->VTOR = 0x20000000;  
   */
 
-  STM_EVAL_LEDInit(LED3);
-  STM_EVAL_LEDInit(LED4);
-  STM_EVAL_LEDInit(LED5);
-  STM_EVAL_LEDInit(LED6);
 
-  NRF radio;//inicializa o NRF com os pinos default, deixa em POWER_UP
-
-  radio.REFRESH();//TODO remover após debug
-
-  radio.TX_configure();//faz uma configuração default para o modo TX
-  radio.REFRESH();//TODO remover após debug
-
-  //inicialização do USB
   USBD_Init(&USB_OTG_dev, USB_OTG_FS_CORE_ID, &USR_desc, &USBD_CDC_cb, &USR_cb);
 
   /* Infinite loop */
   while (1)
   {
-			if(USB_get_and_send(&radio)){
-					STM_EVAL_LEDToggle(LED6);//AZUL:indicador de sucesso
-			}
-			else{
-					STM_EVAL_LEDToggle(LED5);//VERMELHO:indicador de falha
-			}
   }
 }
 
-uint8_t USB_get_and_send(NRF* radio_ptr){
-	  uint8_t  symbol;
-	  uint8_t i;
-	  uint8_t  buffer[]={0,0,0,0,0};
-	  while (1)
-	  {
-		if(VCP_get_char(&symbol))//se RECEBE um char a partir do computador
-		{
-			//verifica se o símbolo recebido indica nova sequência de velocidades,
-			//caso afirmativo, os próximos 4 uint8_t são tratados como velocidades e são enviados
-			if(symbol == 'a'){//TODO: change to 0xff
-			start_of_buffering://start of buffering
-				i=0;
-				buffer[i]='a';//TODO: change to 0xff
-				while (i<4){
-					if(VCP_get_char(&symbol)){
-						if(symbol == 'a')//TODO: change to 0xff
-							goto start_of_buffering;
-						buffer[i+1] = symbol;//preenche o buffer
-						i++;
-					}
-				}
-				VCP_send_buffer(buffer,5);//TODO remover após debug
-				if(radio_ptr->SEND(buffer)){
-						return 1;//indicador de sucesso
-				}
-				else{
-						return 0;//indicador de falha
-				}
-			}
-		}
-	  }
-}
-
-/*void Delay( uint32_t nTime)
-{
-  TimingDelay = nTime;
-
-  while(TimingDelay != 0); //gustavo
-}*/
-
-/*
-void TimingDelay_Decrement(void)
-{
-  if (TimingDelay != 0x00)
-  {
-    TimingDelay--;
-  }
-}
-extern "C" {
-void SysTick_Handler(void)
-{
-  TimingDelay_Decrement();
-}
-}
-*/
 
 void OTG_FS_IRQHandler(void)
 {
